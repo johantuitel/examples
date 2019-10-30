a0ecd8e893f4a175b3e8efab051287fad6a3bfc1
Johan Tuitel <j.tuitel@portbase.com>
2019-10-30 13:18:54 +0100

    bugfix, we need to add dispatch: false to make the effect not return an action

--- weatherradar/weatherradar-web/src/app/store/effect/weather.effect.ts
+++ weatherradar/weatherradar-web/src/app/store/effect/weather.effect.ts
@@ -1,42 +1,43 @@
 import {Actions, Effect, ofType} from '@ngrx/effects';
 import {Injectable} from '@angular/core';
-import {catchError, map, switchMap} from 'rxjs/operators';
-import {Observable, of} from 'rxjs';
+import {catchError, map, switchMap, tap} from 'rxjs/operators';
+import {of} from 'rxjs';
 import {WeatherService} from '../service/weather.service';
 import {LoadWeather, LoadWeatherFailure, LoadWeatherSuccess, StartLiveWeatherFeed, StopLiveWeatherFeed} from '../action/weather.actions';
 import {WeatherActionsType} from '../type/weather.action.type';
 
 @Injectable()
 export class WeatherEffect {
-  constructor(
-    private actions$: Actions,
-    private weatherService: WeatherService
-  ) {}
+    constructor(
+        private actions$: Actions,
+        private weatherService: WeatherService
+    ) {
+    }
 
-  @Effect()
-  getWeather$ = this.actions$.pipe(
-    ofType<LoadWeather>(WeatherActionsType.LOAD_WEATHER),
-      switchMap(() => {
-        return this.weatherService.loadWeather().pipe(
-          map(weather => new LoadWeatherSuccess(weather),
-            catchError(error => of(new LoadWeatherFailure(error))))
-        );
-      })
-  );
+    @Effect()
+    getWeather$ = this.actions$.pipe(
+        ofType<LoadWeather>(WeatherActionsType.LOAD_WEATHER),
+        switchMap(() => {
+            return this.weatherService.loadWeather().pipe(
+                map(weather => new LoadWeatherSuccess(weather),
+                    catchError(error => of(new LoadWeatherFailure(error))))
+            );
+        })
+    );
 
-  @Effect()
-  startLiveWeatherFeed$ = this.actions$.pipe(
-    ofType<StartLiveWeatherFeed>(WeatherActionsType.START_LIVE_WEATHER_FEED),
-    switchMap( () => {
-    return this.weatherService.startLiveWeatherFeed();
-    })
-  );
+    @Effect({dispatch: false})
+    startLiveWeatherFeed$ = this.actions$.pipe(
+        ofType<StartLiveWeatherFeed>(WeatherActionsType.START_LIVE_WEATHER_FEED),
+        tap(() => {
+            this.weatherService.startLiveWeatherFeed();
+        })
+    );
 
-  @Effect()
-  stopLiveWeatherFeed$ = this.actions$.pipe(
-    ofType<StopLiveWeatherFeed>(WeatherActionsType.STOP_LIVE_WEATHER_FEED),
-    switchMap( () => {
-      return this.weatherService.stopLiveWeatherFeed();
-    })
-  );
+    @Effect({dispatch: false})
+    stopLiveWeatherFeed$ = this.actions$.pipe(
+        ofType<StopLiveWeatherFeed>(WeatherActionsType.STOP_LIVE_WEATHER_FEED),
+        tap(() => {
+            return this.weatherService.stopLiveWeatherFeed();
+        })
+    );
 }
